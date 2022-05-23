946808ad8f8a12dc85c3e9765c4d1d05ace9d113
HDubleA <104147218+HDubleA@users.noreply.github.com>
2022-05-23 09:48:38 +0200

    Create setup.py

---
+++ Progetto Colab/setup.py
@@ -0,0 +1,61 @@
+from setuptools import setup, find_packages
+
+from my_pip_package import __version__
+
+extra_math = [
+    'returns-decorator',
+]
+
+extra_bin = [
+    *extra_math,
+]
+
+extra_test = [
+    *extra_math,
+    'pytest>=4',
+    'pytest-cov>=2',
+]
+extra_dev = [
+    *extra_test,
+]
+
+extra_ci = [
+    *extra_test,
+    'python-coveralls',
+]
+
+setup(
+    name='specialized_vader',
+    version=__version__,
+    description='prova prova',
+
+    url='https://github.com/MichaelKim0407/tutorial-pip-package',
+    author='Vittorio Haardt',
+    author_email='vittoriohaardt@gmail.com',
+
+    packages=find_packages(),
+
+    extras_require={
+        'math': extra_math,
+
+        'bin': extra_bin,
+
+        'test': extra_test,
+        'dev': extra_dev,
+
+        'ci': extra_ci,
+    },
+
+    entry_points={
+        'console_scripts': [
+            'add=my_pip_package.math:cmd_add',
+        ],
+    },
+
+    classifiers=[
+        'Intended Audience :: Developers',
+
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+)
