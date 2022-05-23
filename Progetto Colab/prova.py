99d5c8396fe62470b51c4534e3c3264eb2d950af
HDubleA <104147218+HDubleA@users.noreply.github.com>
2022-05-23 09:50:32 +0200

    Update prova.py

--- Progetto Colab/prova.py
+++ Progetto Colab/prova.py
@@ -1,15 +1,7 @@
-# coding: utf-8
-# Author: C.J. Hutto
-# Thanks to George Berry for reducing the time complexity from something like O(N^4) to O(N).
-# Thanks to Ewan Klein and Pierpaolo Pantone for bringing VADER into NLTK. Those modifications were awesome.
-# For license information, see LICENSE.TXT
+__version__ = '1.1'
 
-"""
-If you use the VADER sentiment analysis tools, please cite:
-Hutto, C.J. & Gilbert, E.E. (2014). VADER: A Parsimonious Rule-based Model for
-Sentiment Analysis of Social Media Text. Eighth International Conference on
-Weblogs and Social Media (ICWSM-14). Ann Arbor, MI, June 2014.
-"""
+
+
 import os
 import re
 import math
